682fa3cd2d9552fc4bd14b82abdc00ef10babec7
Zhi Wei Chong <oheyzhiwei@gmail.com>
2022-04-09 06:38:25 +0800

    Properly separate constraints from table to avoid duplication

--- src/main/java/com/google/cloud/solutions/spannerddl/parser/ASTcreate_table_statement.java
+++ src/main/java/com/google/cloud/solutions/spannerddl/parser/ASTcreate_table_statement.java
@@ -26,6 +26,7 @@ import java.util.Optional;
 
 /** Abstract Syntax Tree parser object for "create_table_statement" token */
 public class ASTcreate_table_statement extends SimpleNode {
+  private boolean withConstraints = true;
 
   public ASTcreate_table_statement(int id) {
     super(id);
@@ -77,6 +78,11 @@ public class ASTcreate_table_statement extends SimpleNode {
     }
   }
 
+  public ASTcreate_table_statement clearConstraints() {
+    this.withConstraints = false;
+    return this;
+  }
+
   @Override
   public String toString() {
     verifyTableElements();
@@ -88,7 +94,10 @@ public class ASTcreate_table_statement extends SimpleNode {
     // append column and constraint definitions.
     List<SimpleNode> tableElements = new ArrayList<>();
     tableElements.addAll(getColumns().values());
-    tableElements.addAll(getConstraints().values());
+
+    if (this.withConstraints) {
+      tableElements.addAll(getConstraints().values());
+    }
     ret.append(Joiner.on(", ").join(tableElements));
     ret.append(") ");
     ret.append(getPrimaryKey());

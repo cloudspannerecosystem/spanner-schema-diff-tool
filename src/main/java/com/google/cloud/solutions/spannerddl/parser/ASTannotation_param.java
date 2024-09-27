63efbd654ebeccecf15ec9f9ed6c9b041f296803
gurminder71 <86370145+gurminder71@users.noreply.github.com>
2024-09-27 06:44:01 -0700

    feat: support annotations in SQL file (#171)
    * feat: support annotations in SQL file
    
    Column Annotations are NOT a feature of Cloud Spanner.
     
    This is an additional feature of the Cloud Spanner Schema parser exclusively in this tool so that users of this tool can add metadata to columns, and have that metadata represented in the parsed schema.
    
    See description in src/main/jjtree-sources/ddl_annotation.jjt
    
    ---------
    
    Co-authored-by: Niel Markwick <nielm@google.com>

---
+++ src/main/java/com/google/cloud/solutions/spannerddl/parser/ASTannotation_param.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.solutions.spannerddl.parser;
+
+import com.google.cloud.solutions.spannerddl.diff.AstTreeUtils;
+
+public class ASTannotation_param extends SimpleNode {
+  public ASTannotation_param(int id) {
+    super(id);
+  }
+
+  public ASTannotation_param(DdlParser p, int id) {
+    super(p, id);
+  }
+
+  public String getKey() {
+    ASTparam_key key = AstTreeUtils.getChildByType(this, ASTparam_key.class);
+    return AstTreeUtils.tokensToString(key);
+  }
+
+  public String getValue() {
+    ASTparam_val val = AstTreeUtils.getOptionalChildByType(this, ASTparam_val.class);
+    if (val != null) {
+      return AstTreeUtils.tokensToString(val);
+    } else {
+      return null;
+    }
+  }
+}
---
+++ src/test/java/com/google/cloud/solutions/spannerddl/parser/DDLAnnotationTest.java
@@ -0,0 +1,76 @@
+package com.google.cloud.solutions.spannerddl.parser;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.fail;
+
+import com.google.cloud.solutions.spannerddl.diff.DdlDiff;
+import com.google.cloud.solutions.spannerddl.diff.DdlDiffException;
+import com.google.cloud.solutions.spannerddl.testUtils.ReadTestDatafile;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.junit.Test;
+
+public class DDLAnnotationTest {
+
+  @Test
+  public void validateAnnotations() throws IOException {
+    Map<String, String> tests = ReadTestDatafile.readDdlSegmentsFromFile("annotations.txt");
+    Map<String, String> expects =
+        ReadTestDatafile.readDdlSegmentsFromFile("expectedAnnotations.txt");
+
+    Iterator<Entry<String, String>> testIt = tests.entrySet().iterator();
+    Iterator<Entry<String, String>> expectedIt = expects.entrySet().iterator();
+
+    while (testIt.hasNext() && expectedIt.hasNext()) {
+      Entry<String, String> test = testIt.next();
+      String expected = expectedIt.next().getValue();
+      String segmentName = test.getKey();
+
+      try {
+        // first get all the annotations without removing the comment prefix
+        List<String> annotations = getTableAnnotations(test.getValue(), false);
+
+        // annotations should be empty
+        assertThat(annotations).isEmpty();
+
+        // now get all the annotations after removing the comment prefix
+        annotations = getTableAnnotations(test.getValue(), true);
+
+        List<String> expectedList =
+            expected != null ? Arrays.asList(expected.split("\n")) : Collections.emptyList();
+
+        assertWithMessage("Mismatch for section " + segmentName)
+            .that(annotations)
+            .isEqualTo(expectedList);
+      } catch (DdlDiffException e) {
+        fail("Failed to parse section: '" + segmentName + "': " + e);
+      }
+    }
+  }
+
+  private List<String> getTableAnnotations(String ddl, boolean parseAnnotations)
+      throws DdlDiffException {
+    List<String> annotations = new ArrayList<>();
+
+    List<ASTddl_statement> statements = DdlDiff.parseDdl(ddl, parseAnnotations);
+    for (ASTddl_statement statement : statements) {
+      if (statement.jjtGetChild(0).getId() == DdlParserTreeConstants.JJTCREATE_TABLE_STATEMENT) {
+        Node tableStatement = statement.jjtGetChild(0);
+        for (int i = 0, count = tableStatement.jjtGetNumChildren(); i < count; i++) {
+          Node child = tableStatement.jjtGetChild(i);
+          if (child instanceof ASTannotation) {
+            annotations.add(((ASTannotation) child).getAnnotation());
+          }
+        }
+      }
+    }
+    return annotations;
+  }
+}
